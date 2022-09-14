67b64ddae3ce8f0eb5e0883d070b9aa47d70b0bc
Finn Lindgren <Finn.Lindgren@gmail.com>
2022-09-14 11:15:26 +0100

    Convert to roxygen2 markdown syntax

--- R/rSPDE.R
+++ R/rSPDE.R
@@ -1,6 +1,6 @@
 #'  Rational approximations of fractional SPDEs.
 #'
-#' \code{rSPDE} is used for approximating fractional elliptic SPDEs
+#' `rSPDE` is used for approximating fractional elliptic SPDEs
 #' \deqn{L^\beta (\tau u(s)) = W,}
 #' where \eqn{L} is a differential operator and \eqn{\beta>0}
 #' is a general fractional power.
@@ -11,24 +11,24 @@
 #'
 #' The main functions for computing rational approximation objects are:
 #' \itemize{
-#' \item{\code{\link{fractional.operators}}}{works for general
+#' \item{[fractional.operators()]}{works for general
 #' rational operators}
-#' \item{\code{\link{matern.operators}}}{ works for random fields with
+#' \item{[matern.operators()]}{ works for random fields with
 #' stationary Matern covariance functions}
-#' \item{\code{\link{spde.matern.operators}}}{ works for random fields with
+#' \item{[spde.matern.operators()]}{ works for random fields with
 #' defined as solutions to a possibly non-stationary Matern-type SPDE model.}
-#' \item{\code{\link{rspde.matern}}} {R-INLA implementation of the
+#' \item{[rspde.matern()]} {R-INLA implementation of the
 #' covariance-based rational approximation for random fields with
 #' stationary Matern covariance functions}
 #' }
 #' Basic statistical operations such as likelihood evaluations (see
-#' \code{\link{rSPDE.loglike}, \link{rSPDE.matern.loglike}}) and kriging
-#' predictions (see \code{\link{predict.rSPDEobj}, \link{predict.CBrSPDEobj}})
+#' `[rSPDE.loglike], [rSPDE.matern.loglike]`) and kriging
+#' predictions (see `[predict.rSPDEobj], [predict.CBrSPDEobj]`)
 #' using the rational approximations are also implemented.
 #'
 #' For illustration purposes, the package contains a simple FEM implementation
 #' for models on R. For spatial models,
-#' the FEM implementation in the \code{R-INLA} package is recommended.
+#' the FEM implementation in the `R-INLA` package is recommended.
 #'
 #' For a more detailed introduction to the package, see the rSPDE Vignettes.
 #'
