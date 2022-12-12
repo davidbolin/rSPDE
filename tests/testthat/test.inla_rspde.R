dc9b9aaaeaca8efe59ccd4485988b73d39409c4d
vpnsctl <alexandre.impa@gmail.com>
2022-12-12 19:15:58 +0300

    Adding tests for nonstationary models

--- tests/testthat/test.inla_rspde.R
+++ tests/testthat/test.inla_rspde.R
@@ -152,3 +152,94 @@ Q_1 <- precision(op)
 
 testthat::expect_equal(sum( (Q_1 - Q_tmp$Q)^2), 0)
 })
+
+
+test_that("testing cgeneric_nonstat_gen", {
+data(PRprec, package = "INLA")
+
+Y <- rowMeans(PRprec[, 3 + 1:31])
+
+ind <- !is.na(Y)
+Y <- Y[ind]
+coords <- as.matrix(PRprec[ind, 1:2])
+
+prdomain <- INLA::inla.nonconvex.hull(coords, -0.03, -0.05, resolution = c(50, 50))
+prmesh <- INLA::inla.mesh.2d(boundary = prdomain, max.edge = c(0.45, 1), cutoff = 0.5)
+
+
+rspde_model <- rspde.matern(mesh = prmesh, parameterization = "spde",
+                       B.tau = cbind(0,1,rep(0,prmesh$n)))
+
+stopifnot(!rspde_model$stationary)
+
+Q_tmp <- INLA::inla.cgeneric.q(rspde_model)
+
+rspde_stat_model <- rspde.matern(mesh = prmesh,
+                            parameterization = "spde")
+
+stopifnot(rspde_stat_model$stationary)
+
+Q_1 <- inla.cgeneric.q(rspde_stat_model)
+
+testthat::expect_equal(sum( (Q_1$Q - Q_tmp$Q)^2), 0)
+})
+
+test_that("testing cgeneric_nonstat_fixed", {
+data(PRprec, package = "INLA")
+
+Y <- rowMeans(PRprec[, 3 + 1:31])
+
+ind <- !is.na(Y)
+Y <- Y[ind]
+coords <- as.matrix(PRprec[ind, 1:2])
+
+prdomain <- INLA::inla.nonconvex.hull(coords, -0.03, -0.05, resolution = c(50, 50))
+prmesh <- INLA::inla.mesh.2d(boundary = prdomain, max.edge = c(0.45, 1), cutoff = 0.5)
+
+
+rspde_model <- rspde.matern(mesh = prmesh, nu = 0.7, parameterization = "spde",
+                       B.tau = cbind(0,1,rep(0,prmesh$n)))
+
+stopifnot(!rspde_model$stationary)
+
+Q_tmp <- INLA::inla.cgeneric.q(rspde_model)
+
+rspde_stat_model <- rspde.matern(mesh = prmesh, nu = 0.7,
+                            parameterization = "spde")
+
+stopifnot(rspde_stat_model$stationary)
+
+Q_1 <- inla.cgeneric.q(rspde_stat_model)
+
+testthat::expect_equal(sum( (Q_1$Q - Q_tmp$Q)^2), 0)
+})
+
+test_that("testing cgeneric_nonstat_integer", {
+data(PRprec, package = "INLA")
+
+Y <- rowMeans(PRprec[, 3 + 1:31])
+
+ind <- !is.na(Y)
+Y <- Y[ind]
+coords <- as.matrix(PRprec[ind, 1:2])
+
+prdomain <- INLA::inla.nonconvex.hull(coords, -0.03, -0.05, resolution = c(50, 50))
+prmesh <- INLA::inla.mesh.2d(boundary = prdomain, max.edge = c(0.45, 1), cutoff = 0.5)
+
+
+rspde_model <- rspde.matern(mesh = prmesh, parameterization = "spde", nu = 1,
+                       B.tau = cbind(0,1,rep(0,prmesh$n)))
+
+stopifnot(!rspde_model$stationary)
+
+Q_tmp <- INLA::inla.cgeneric.q(rspde_model)
+
+rspde_stat_model <- rspde.matern(mesh = prmesh, nu = 1,
+                            parameterization = "spde")
+
+stopifnot(rspde_stat_model$stationary)
+
+Q_1 <- inla.cgeneric.q(rspde_stat_model)
+
+testthat::expect_equal(sum( (Q_1$Q - Q_tmp$Q)^2), 0)
+})
